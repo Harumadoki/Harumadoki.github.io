b17834992be65cf3f1531a6ef1432c4e7c0f1938
Rtine <rasoanaivo.heritiana@gmail.com>
2025-04-04 21:42:35 +0400

    🔨: handling mobile presentation for personnal project details

--- rtine/src/components/ProjectDetails/ProjectDetails.tsx
+++ rtine/src/components/ProjectDetails/ProjectDetails.tsx
@@ -22,12 +22,23 @@ interface ProjectDetailsProps {
 const ProjectDetails: React.FC<ProjectDetailsProps> = ({
   experienceId,
 }: ProjectDetailsProps) => {
+  const MOBILE_SIZE_MAX = 1024;
+  const [isMobile, setIsMobile] = useState(
+    window.innerWidth <= MOBILE_SIZE_MAX
+  );
   const [experience, setExperience] = useState<ExperienceType | null>(null);
 
   useEffect(() => {
     setExperience(
       experiencesData.find((exp) => exp.id === experienceId) || null
     );
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= MOBILE_SIZE_MAX);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, [experienceId]);
 
   return (
@@ -35,29 +46,43 @@ const ProjectDetails: React.FC<ProjectDetailsProps> = ({
       <h2>{experience?.title}</h2>
       <p className="introduction">{experience?.introduction}</p>
       <div className="sections">
-        {experience?.sections.map((section, index) => {
-          return (
-            <div key={section.title} className="section">
-              {index % 2 === 0 ? (
-                <>
-                  <img className="image" src={section.image}></img>
-                  <div className="description">
-                    <h3>{section.title}</h3>
-                    <p>{section.description}</p>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <div className="description">
-                    <h3>{section.title}</h3>
-                    <p>{section.description}</p>
-                  </div>
-                  <img className="image" src={section.image}></img>
-                </>
-              )}
-            </div>
-          );
-        })}
+        {isMobile
+          ? experience?.sections.map((section, index) => {
+              return (
+                <div key={section.title} className="section">
+                  <>
+                    <img className="image" src={section.image}></img>
+                    <div className="description">
+                      <h3>{section.title}</h3>
+                      <p>{section.description}</p>
+                    </div>
+                  </>
+                </div>
+              );
+            })
+          : experience?.sections.map((section, index) => {
+              return (
+                <div key={section.title} className="section">
+                  {index % 2 === 0 ? (
+                    <>
+                      <img className="image" src={section.image}></img>
+                      <div className="description">
+                        <h3>{section.title}</h3>
+                        <p>{section.description}</p>
+                      </div>
+                    </>
+                  ) : (
+                    <>
+                      <div className="description">
+                        <h3>{section.title}</h3>
+                        <p>{section.description}</p>
+                      </div>
+                      <img className="image" src={section.image}></img>
+                    </>
+                  )}
+                </div>
+              );
+            })}
       </div>
     </div>
   );
