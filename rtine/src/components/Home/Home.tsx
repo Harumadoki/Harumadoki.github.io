2b1d6059c82beb8b68f3773b91103f7ff87672b6
Rtine <rasoanaivo.heritiana@gmail.com>
2025-05-16 22:26:23 +0900

    🔨: location modified

--- rtine/src/components/Home/Home.tsx
+++ rtine/src/components/Home/Home.tsx
@@ -12,7 +12,7 @@ const Introduction = () => {
         in <strong>React, Angular and .NET</strong>.
         <br /> <br />I love building web applications, solving complex problems,
         and working in <strong>Agile teams</strong>. Currently based in{" "}
-        <strong>Tokyo</strong>, I'm always looking for new challenges and
+        <strong>Paris</strong>, I'm always looking for new challenges and
         opportunities to grow.
       </p>
       <p>
